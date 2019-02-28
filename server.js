40b890010b05539856370f62dd1da001c3131eab
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-02-28 11:12:22 -0300

    change server to start from node

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-'use strict'
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
 
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
-  })
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
 }
+start()
